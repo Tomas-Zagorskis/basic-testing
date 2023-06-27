d7fd708f707e3675994eb9ff923ed8a105108478
Tomas Zagorskis <tomzagos@gmail.com>
2023-06-27 17:52:34 +0300

    feat: write error handling tests

--- src/03-error-handling-async/index.test.ts
+++ src/03-error-handling-async/index.test.ts
@@ -1,30 +1,66 @@
-// Uncomment the code below and write your tests
-// import { throwError, throwCustomError, resolveValue, MyAwesomeError, rejectCustomError } from './index';
+import {
+  throwError,
+  throwCustomError,
+  resolveValue,
+  MyAwesomeError,
+  rejectCustomError,
+} from './index';
 
 describe('resolveValue', () => {
   test('should resolve provided value', async () => {
-    // Write your test here
+    expect.assertions(1);
+
+    expect(await resolveValue(5)).toBe(5);
   });
 });
 
 describe('throwError', () => {
   test('should throw error with provided message', () => {
-    // Write your test here
+    expect.assertions(2);
+
+    try {
+      throwError('custom message');
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty('message', 'custom message');
+    }
   });
 
   test('should throw error with default message if message is not provided', () => {
-    // Write your test here
+    expect.assertions(2);
+
+    try {
+      throwError();
+    } catch (error) {
+      expect(error).toBeInstanceOf(Error);
+      expect(error).toHaveProperty('message', 'Oops!');
+    }
   });
 });
 
 describe('throwCustomError', () => {
   test('should throw custom error', () => {
-    // Write your test here
+    expect.assertions(2);
+
+    try {
+      throwCustomError();
+    } catch (error) {
+      expect(error).toBeInstanceOf(MyAwesomeError);
+      expect(error).toHaveProperty(
+        'message',
+        'This is my awesome custom error!',
+      );
+    }
   });
 });
 
 describe('rejectCustomError', () => {
   test('should reject custom error', async () => {
-    // Write your test here
+    expect.assertions(2);
+    await expect(rejectCustomError()).rejects.toBeInstanceOf(MyAwesomeError);
+    await expect(rejectCustomError()).rejects.toHaveProperty(
+      'message',
+      'This is my awesome custom error!',
+    );
   });
 });
